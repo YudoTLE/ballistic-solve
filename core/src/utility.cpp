ed9f3126082be11ae2f8967f69187e83b4060c22
YudoTLE <yudojago@gmail.com>
2026-02-01 15:52:59 +0700

    fix: critical bug, flipped x and y axis for to_direction utility

--- core/src/utility.cpp
+++ core/src/utility.cpp
@@ -5,9 +5,9 @@ namespace ballistic_solve
     Eigen::Vector3d to_direction(const Eigen::Vector2d &angles)
     {
         return Eigen::Vector3d(
-            std::cos(angles.x()) * std::cos(angles.y()),
-            std::cos(angles.x()) * std::sin(angles.y()),
-            std::sin(angles.x()));
+            std::cos(angles.y()) * std::cos(angles.x()),
+            std::cos(angles.y()) * std::sin(angles.x()),
+            std::sin(angles.y()));
     }
 
     Eigen::Vector2d to_angles(const Eigen::Vector3d &direction)
